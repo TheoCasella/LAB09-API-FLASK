1773bbeb5ae14384a910711793c233818701437c
TheoCasella <theocasella@gmail.com>
2026-05-15 20:07:50 -0300

    Update restful_api.py

--- restful_api.py
+++ restful_api.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/api/hello', methods=['GET'])
-def hello_world():
-    return {
-        "mensagem": "Hello, World!",
-        "status": "sucesso"
-    }
+mensagem = "Hello, World!"
+
+@app.route('/api', methods=['GET'])
+def get_msg():
+    return {"conteudo": mensagem}
+
+@app.route('/api', methods=['POST'])
+def post_msg():
+    global mensagem
+    dados = request.get_json()
+    mensagem = dados.get("texto")
+    return {"aviso": "Mensagem criada!", "nova_mensagem": mensagem}, 201
+
+@app.route('/api', methods=['PUT'])
+def put_msg():
+    global mensagem
+    dados = request.get_json()
+    mensagem = dados.get("texto")
+    return {"aviso": "Mensagem atualizada!", "nova_mensagem": mensagem}
+
+@app.route('/api', methods=['DELETE'])
+def delete_msg():
+    global mensagem
+    mensagem = ""
+    return {"aviso": "Mensagem apagada!"}
 
 if __name__ == '__main__':
     app.run(debug=True)
